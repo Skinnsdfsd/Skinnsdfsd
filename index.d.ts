8c3369beabbd31a2745abf04cd7045bb9b8f2500
Skinnsdfsd <42322006@mt.peradaban.ac.id>
2024-12-20 01:30:13 +0000

    feat: 增加字段

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,12 @@ export interface iSelf {
     age: number;
     /** 地址 */
     address: string;
+    /** 电话 */
+    phone: string;
+    /** 邮箱 */
+    email: string;
+    /** 性别 **/
+    sex: string;
+    /** 个人简介 */
+    desc: string;
 }
