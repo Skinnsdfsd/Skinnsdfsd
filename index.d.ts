e90fea8e766899c109ad78d1c99f5f402b958ebc
Skinnsdfsd <w852174914@gmail.com>
2025-02-07 01:10:31 +0000

    feat: remove some code

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,4 @@
 export interface iSelf {
     name: string;
     age: number;
-    /** 地址 */
-    address: string;
-    /** 电话 */
-    phone: string;
-    /** 邮箱 */
-    email: string;
-    /** 性别 **/
-    sex: string;
-    /** 个人简介 */
-    desc: string;
 }
